a7ce3a949e0b93596109b9766356bff5d280f5f5
theotherstupidguy <theotherstupidguy@gmail.com>
2014-05-17 16:02:19 +0200

    next ver

--- lib/gameon/base.rb
+++ lib/gameon/base.rb
@@ -3,7 +3,6 @@ module GameOn
     GameOn::Persistence::DS.load id
   end
 
-  
   module Gamebook
     extend Mushin::DSL::Notebook 
     Mushin::DSL::Notebook.build 'game', 'rule', 'activate' 
@@ -13,39 +12,5 @@ module GameOn
 
   class Env < Mushin::Env
     @@ds = 'GameOn::Persistence::DS'
-    #Mushin::Engine.setup [GameOn::Persistence::DS]
   end
-=begin
-  module Engine
-    def Engine.run game, activity
-      Mushin::Engine.setup [GameOn::Persistence::DS]
-      Mushin::Engine.run game, activity
-    end
-  end
-=end
-
-  #class Activities < Mushin::DSL::Activities
-  #include Mushin::DSL::Activities
-  #extend Mushin::DSL::Activities
-  #TODO transfer construction to mushin
-  #def self.construction game, activity   
-  #  GameOn::Engine.run game, activity   
-  #end
-  #end
- 
-=begin
-  module Engine
-    def Engine.run game, activity
-      #[Mushin::Presistence::DS, Mushin::blahblah]
-      #use Mushin::Persistence::DS
-      #Mushin::Engine.setup do
-	#['some entity middleware','some datastore'] 
-	#use Mushin::DS.blaa and then class_eval there
-      #end
-      Mushin::Engine.setup [GameOn::Persistence::DS]
-      Mushin::Engine.run game, activity
-    end
-  end
-=end
-
 end
