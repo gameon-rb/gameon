19a98f5bcf6ce8cf6abf91416f062b03bd3728d7
theotherstupidguy <theotherstupidguy@gmail.com>
2014-05-28 21:19:14 +0200

    next ver

--- lib/gameon/base.rb
+++ lib/gameon/base.rb
@@ -1,21 +1,79 @@
 module GameOn
-  module Gamebook
-    extend Mushin::DSL::Notebook 
-    #Mushin::DSL::Notebook.build [:game], [:rule,:2ndlevelconstruct,[:nested_constructA, :nested_contructB]], [:activate] 
-    #Mushin::DSL::Notebook.build 'game', 'rule', 'activate' 
-    #TODO As a Framework developer you get to add alias_methods
-    # Example  alias_method :ninja, :rule
+
+  module Middleware 
+    include Mushin::Domain::Middleware
   end
 
-  class Middleware 
-    include Mushin::Customization 
+  module DSL 
+    include Mushin::DSL
+    def self.find activity_context, activity_statment
+      Mushin::DSL.middlewares = []
+      Mushin::DSL.contexts.each do |current_context|
+	if activity_context == current_context.title
+	  current_context.statments.each do |statment|
+	    if activity_statment == statment.title
+	      statment.activations.each do |middleware|
+		Mushin::DSL.middlewares << middleware 
+	      end
+	    end
+	  end
+	end
+      end
+      Mushin::DSL.middlewares
+    end
   end
 
-  class Env < Mushin::Env
-    @@domain_folder = "gameon"
-    @@ds = 'GameOn::Persistence::DS'
-    def Env.get id
-      GameOn::Persistence::DS.load id
+  module Engine
+    extend Mushin::Engine
+    attr_accessor :middlewares, :stack
+    def self.run domain_context, activity
+      @@middlewares = GameOn::DSL.find domain_context, activity 
+      @stack = Mushin::Middleware::Builder.new do
+	@@middlewares.each do |middleware|
+	  p "Mushin Logging: use #{middleware.name}, #{middleware.opts}, #{middleware.params}"
+	  use middleware.name, middleware.opts, middleware.params
+	end
+      end
+      @setup_middlewares.each do |setup_middleware|
+	@stack.insert_before 0, setup_middleware 
+      end
+      @stack.call
+      @@middlewares = []
+    end
+  end
+
+  #module Env 
+  class Env 
+    extend Mushin::Env
+
+    class << self
+      attr_accessor :id
+
+      def get id
+	GameOn::Persistence::DS.load id.to_s + 'gameon'
+      end
+
+      def set id, &block 
+	@id = id.to_s + 'gameon' 
+	def on domain_context, &block
+	  @domain_context = domain_context 
+	  @activities = []  
+	  def activity statment 
+	    @activities += [statment]                                                                          
+	  end
+	  instance_eval(&block)
+	end
+	instance_eval(&block)
+
+	Dir["./gameon/*"].each {|file| load file }  
+
+	GameOn::Engine.setup [Object.const_get('GameOn::Persistence::DS')]
+	@activities.each do |activity| 
+	  GameOn::Engine.run @domain_context, activity   
+	end
+	return GameOn::Persistence::DS.load @id 
+      end
+      #alias_method :[], :set
     end
   end
 end
